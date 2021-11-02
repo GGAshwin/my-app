b5e9bc49e313cc637c55409d2392c855cc2b5854
playmaker159 <ashwinprabhu2001@gmail.com>
2021-11-02 09:56:26 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
 
 function App() {
+  const [darkMode, setdarkMode] = useState('light')//weather dark mode is enabled or not
   return (
     <>
-    <Navbar />
+    <Navbar mode={darkMode}/>
     <TextArea content="Type something"/>
     <About />
     </>
